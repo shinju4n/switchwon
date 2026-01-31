6f50a1a53b28e82842b61092562ff96769f10b06
ju4n <ju4nshin@gmail.com>
2026-02-01 01:28:58 +0900

    feat: 컴포넌트 높이 수정

--- components/custom/custom-input.tsx
+++ components/custom/custom-input.tsx
@@ -14,10 +14,13 @@ export const CustomInput = ({
 }: CustomInputProps) => {
   return (
     <div className="flex flex-col gap-2">
-      <label className="text-[15px] font-medium text-[#646F7C]">{label}</label>
+      <label className="text-[20px] font-medium text-[#646F7C]">{label}</label>
       <Input
         {...inputOptions}
-        className={cn('p-6 text-[20px] font-semibold', inputOptions.className)}
+        className={cn(
+          'h-auto rounded-[12px] border-[#374553] p-6 text-[20px] font-semibold text-[#646F7C] placeholder:text-[20px] md:text-[20px]',
+          inputOptions.className
+        )}
       />
       {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
     </div>
