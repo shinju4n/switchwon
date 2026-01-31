3554a707b71cf54799a4d15df1e461793eb38648
ju4n <ju4nshin@gmail.com>
2026-01-31 22:40:05 +0900

    feat: tanstack query 401 에러 처리

---
+++ app/provider.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { useState } from 'react';
+import { Toaster } from '@/components/ui/sonner';
+
+import { ApiError } from '@/lib/api';
+
+const handleGlobalError = (error: Error) => {
+  if (error instanceof ApiError && error.status === 401) {
+    const { pathname, search } = window.location;
+    const fullPath = search ? `${pathname}${search}` : pathname;
+    const returnTo = encodeURIComponent(fullPath);
+    window.location.href = `/login?redirect=${returnTo}`;
+  }
+};
+
+export const Provider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            refetchOnWindowFocus: false,
+          },
+        },
+        queryCache: new QueryCache({
+          onError: handleGlobalError,
+        }),
+        mutationCache: new MutationCache({
+          onError: handleGlobalError,
+        }),
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Toaster />
+    </QueryClientProvider>
+  );
+};
